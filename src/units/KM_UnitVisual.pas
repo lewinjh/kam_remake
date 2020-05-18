fe36c0d2792abd4a6d5d31a8b531ad99e2fced32
Lewin <Hodgman>
2020-05-18 20:25:58 +1000

    Generate fractional AnimStep where possible

--- src/units/KM_UnitVisual.pas
+++ src/units/KM_UnitVisual.pas
@@ -11,10 +11,9 @@ type
     SlideX, SlideY: Single;
     Action: TKMUnitActionType;
     AnimStep: Integer;
+    AnimFraction: Single;
 
     procedure SetFromUnit(aUnit: TObject);
-
-    class function Lerp(const A, B: TKMUnitVisualState; Mix: Single): TKMUnitVisualState; static;
   end;
 
   // Purely visual thing. Split from TKMUnit to aviod mixup of game-logic and render Positions
@@ -48,31 +47,12 @@ begin
   SlideX := U.GetSlide(axX);
   SlideY := U.GetSlide(axY);
   AnimStep := U.AnimStep;
+  AnimFraction := 0.0;
 
   if U.Action <> nil then
     Action := U.Action.ActionType
   else
     Action := uaUnknown;
-end;
-
-
-class function TKMUnitVisualState.Lerp(const A, B: TKMUnitVisualState; Mix: Single): TKMUnitVisualState;
-begin
-  Result.PosF := KMLerp(A.PosF, B.PosF, Mix);
-  Result.SlideX := KromUtils.Lerp(A.SlideX, B.SlideX, Mix);
-  Result.SlideY := KromUtils.Lerp(A.SlideY, B.SlideY, Mix);
-  if Mix < 0.5 then
-  begin
-    Result.Dir := A.Dir;
-    Result.AnimStep := A.AnimStep;
-    Result.Action := A.Action;
-  end
-  else
-  begin
-    Result.Dir := B.Dir;
-    Result.AnimStep := B.AnimStep;
-    Result.Action := B.Action;
-  end;
 end;
 
 
@@ -88,7 +68,28 @@ end;
 
 function TKMUnitVisual.GetLerp(aLag: Single): TKMUnitVisualState;
 begin
-  Result := TKMUnitVisualState.Lerp(Curr, Prev, aLag);
+  Result.PosF := KMLerp(Curr.PosF, Prev.PosF, aLag);
+  Result.SlideX := KromUtils.Lerp(Curr.SlideX, Prev.SlideX, aLag);
+  Result.SlideY := KromUtils.Lerp(Curr.SlideY, Prev.SlideY, aLag);
+  //If there's no lag, use the current state
+  if aLag = 0.0 then
+  begin
+    Result.Dir := Curr.Dir;
+    Result.Action := Curr.Action;
+    Result.AnimStep := Curr.AnimStep;
+    Result.AnimFraction := 0.0;
+  end
+  else
+  begin
+    //Don't start a new action or change direction until the last one is 100% finished
+    Result.Dir := Prev.Dir;
+    Result.Action := Prev.Action;
+    Result.AnimStep := Prev.AnimStep;
+    Result.AnimFraction := 0.0;
+    //If action/dir/step is consistent we can interpolate the animation
+    if (Curr.Action = Prev.Action) and (Curr.Dir = Prev.Dir) and (Curr.AnimStep - Prev.AnimStep = 1) then
+      Result.AnimFraction := 1.0 - aLag;
+  end;
 end;
 
 
